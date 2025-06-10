95a2bc6ddb64fd5390897f0dced44209a02a6ddb
PallabiPM <pallabim26@iimklive.com>
2025-06-11 03:01:36 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update
+apt-get install -y wget curl unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt-get install -f -y
